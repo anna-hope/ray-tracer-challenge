10a35b100f2c88f8bdaddda292726959c73ff0de
Anna Melnikov <anna@caravelapp.com>
2023-03-17 11:49:31 -0500

    refactor: move one-off code to main.rs

--- ray_tracer/src/lib.rs
+++ ray_tracer/src/lib.rs
@@ -6,19 +6,3 @@ mod tuple;
 
 pub use matrix::Matrix;
 pub use tuple::Tuple;
-
-pub struct Projectile {
-    pub position: Tuple,
-    pub velocity: Tuple,
-}
-
-pub struct Environment {
-    pub gravity: Tuple,
-    pub wind: Tuple,
-}
-
-pub fn tick(env: &Environment, proj: &Projectile) -> Projectile {
-    let position = proj.position + proj.velocity;
-    let velocity = proj.velocity + env.gravity + env.wind;
-    Projectile { position, velocity }
-}

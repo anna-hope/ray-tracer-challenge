fe2241320e19cd7529b684785dc37a00ae98a49f
Anna Melnikov <anna@caravelapp.com>
2023-03-21 10:39:20 -0700

    docs(material): add doc comment for default
    Add doc comment for Material::default.

--- ray_tracer/src/material.rs
+++ ray_tracer/src/material.rs
@@ -82,6 +82,9 @@ impl Material {
 }
 
 impl Default for Material {
+    /// Initialize material with the default parameters:
+    /// color = white, ambient = 0.1, diffuse = 0.9, specular = 0.9
+    /// shininess = 200.
     fn default() -> Self {
         let color = Color::white();
         let ambient = 0.1;

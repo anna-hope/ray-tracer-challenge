99773978e65fd5b7a4661ccaecb60534e26ff8bb
Anna Melnikov <anna@caravelapp.com>
2023-03-21 19:39:57 -0700

    feat: add `color_at`
    Add method to compute color at the given intersection of ray and world.

--- ray_tracer/src/world.rs
+++ ray_tracer/src/world.rs
@@ -1,5 +1,5 @@
 use crate::{
-    intersection::{Intersect, Intersection},
+    intersection::{hit, Computations, Intersect, Intersection},
     light::PointLight,
     material::Material,
     sphere::Sphere,
@@ -18,6 +18,35 @@ impl World {
         Self {
             objects: vec![],
             light: None,
+        }
+    }
+
+    /// Returns the color at the intersection encapsulated by comps,
+    /// in the given world. Returns black if the world has no light source.
+    fn shade_hit(&self, comps: &Computations) -> Color {
+        // possible future improvement: support multiple light sources
+        // (p. 96 of the book)
+        if let Some(light) = self.light {
+            comps.object.material().lighting(
+                light,
+                comps.point,
+                comps.eye_vector,
+                comps.normal_vector,
+            )
+        } else {
+            Color::default()
+        }
+    }
+
+    /// Intersects the world with the given ray and returns
+    /// the color at the resulting intersection.
+    pub fn color_at(&self, ray: &Ray) -> Color {
+        let xs = self.intersect(ray);
+        if let Some(hit) = hit(&xs) {
+            let comps = hit.prepare_computations(ray);
+            self.shade_hit(&comps)
+        } else {
+            Color::default()
         }
     }
 }
@@ -109,5 +138,72 @@ mod tests {
         assert_eq!(xs[1].t, 4.5);
         assert_eq!(xs[2].t, 5.5);
         assert_eq!(xs[3].t, 6.);
+    }
+
+    #[test]
+    fn shading_intersection() {
+        let world = World::default();
+        let ray = Ray::new(Tuple::point(0., 0., -5.), Tuple::vector(0., 0., 1.));
+        let shape = &world.objects[0];
+        let intersection = shape.arbitrary_intersection(4.);
+        let comps = intersection.prepare_computations(&ray);
+        let color = world.shade_hit(&comps);
+        assert_eq!(color, Color::new(0.38066, 0.47583, 0.2855));
+    }
+
+    #[test]
+    fn shading_intersection_from_inside() {
+        let mut world = World::default();
+        world.light = Some(PointLight::new(Tuple::point(0., 0.25, 0.), Color::white()));
+
+        let ray = Ray::new(Tuple::point(0., 0., 0.), Tuple::vector(0., 0., 1.));
+
+        let shape = &world.objects[1];
+        let intersection = shape.arbitrary_intersection(0.5);
+        let comps = intersection.prepare_computations(&ray);
+        let color = world.shade_hit(&comps);
+
+        let val = 0.90498;
+        assert_eq!(color, Color::new(val, val, val));
+    }
+
+    #[test]
+    fn color_when_ray_misses() {
+        // when the ray fails to intersect anything, the returned color should be black
+        let world = World::default();
+        let ray = Ray::new(Tuple::point(0., 0., -5.), Tuple::vector(0., 1., 0.));
+        let color = world.color_at(&ray);
+        assert_eq!(color, Color::default());
+    }
+
+    #[test]
+    fn color_when_ray_hits() {
+        // our ray intersects the outermost sphere in the default world
+        let world = World::default();
+        let ray = Ray::new(Tuple::point(0., 0., -5.), Tuple::vector(0., 0., 1.));
+        let color = world.color_at(&ray);
+        assert_eq!(color, Color::new(0.38066, 0.47583, 0.2855));
+    }
+
+    #[test]
+    fn color_with_intersection_behind_ray() {
+        // color_at should use `hit` when computing the color
+        // We put the ray inside the outer sphere, but outside the inner sphere,
+        // pointing at the inner sphere.
+        // We expect the hit to be on the inner sphere, and so return its color
+        let mut world = World::default();
+        let outer = &mut world.objects[0];
+        let mut outer_material = outer.material();
+        outer_material.ambient = 1.;
+        outer.set_material(outer_material);
+
+        let inner = &mut world.objects[1];
+        let mut inner_material = inner.material();
+        inner_material.ambient = 1.;
+        inner.set_material(inner_material);
+
+        let ray = Ray::new(Tuple::point(0., 0., 0.75), Tuple::vector(0., 0., -1.));
+        let color = world.color_at(&ray);
+        assert_eq!(color, inner_material.color);
     }
 }

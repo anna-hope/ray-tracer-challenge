cef97dc46af7ea7e6e553e06bafe5d11529d040a
Anna Melnikov <anna.me@hey.com>
2023-04-06 23:49:31 -0500

    fix: write output file to working directory
    Write the output file to the working directory.

--- rt_run/src/main.rs
+++ rt_run/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let yaml_filename = args
         .nth(1)
         .expect("Must provide a path to a YAML file with a scene description.");
-    let mut file = File::open(yaml_filename).expect("The file should exist and be openable");
+    let mut file = File::open(&yaml_filename).expect("The file should exist and be openable");
     let mut data = String::new();
     file.read_to_string(&mut data)
         .expect("The file should be readable to a string");
@@ -28,6 +28,17 @@ fn main() {
         .expect("Should be able to render the scene");
 
     let ppm = canvas.to_ppm();
-    let mut buffer = File::create("scene.ppm").unwrap();
+
+    let filename_stem = yaml_filename
+        .split(".")
+        .nth(0)
+        .unwrap()
+        .split("/")
+        .last()
+        .unwrap();
+    let output_filename = format!("{filename_stem}.ppm",);
+    let mut buffer = File::create(&output_filename).unwrap();
     buffer.write_all(ppm.as_bytes()).unwrap();
+
+    println!("Wrote output to {output_filename}");
 }

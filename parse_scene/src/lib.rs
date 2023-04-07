61ddec123b597fa33e0ffa185320043f57de9161
Anna Melnikov <anna.me@hey.com>
2023-04-06 19:29:27 -0500

    feat(parser): implement scene parsing
    Implement scene parsing from YAML files in the format given by the book.

---
+++ parse_scene/src/lib.rs
@@ -0,0 +1,292 @@
+use serde_yaml::{Mapping, Sequence, Value};
+use thiserror::Error;
+
+use ray_tracer::{
+    camera::Camera, light::Light, material::Material, shape::*,
+    transformation::compute_view_transformation, Color, Matrix, Tuple,
+};
+
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("YAML deserialization error: {0}")]
+    Yaml(#[from] serde_yaml::Error),
+
+    #[error("Ray tracer error: {0}")]
+    RayTracer(#[from] ray_tracer::RayTracerError),
+
+    #[error("Invalid YAML object: {0}")]
+    InvalidYamlObject(String),
+
+    #[error("Encountered unknown item: {0}")]
+    UnknownItem(String),
+
+    #[error("Invalid parameters for camera: {0}")]
+    InvalidCameraParams(String),
+
+    #[error("Invalid parameters for light: {0}")]
+    InvalidLightParams(String),
+
+    #[error("Invalid parameters for shape: {0}")]
+    InvalidShapeParams(String),
+
+    #[error("Invalid value")]
+    InvalidValue,
+}
+
+pub type Result<T> = std::result::Result<T, ParseError>;
+
+macro_rules! get_or_default_f64 {
+    ($mapping:expr, $key:expr, $default:expr) => {
+        $mapping
+            .get($key)
+            .and_then(|x| x.as_f64())
+            .unwrap_or($default)
+    };
+}
+
+#[derive(Debug)]
+pub struct Scene {
+    pub camera: Camera,
+    pub lights: Vec<Light>,
+    pub objects: Vec<Box<dyn Shape>>,
+}
+
+trait GetF64 {
+    fn get_f64(&self) -> Result<f64>;
+}
+
+trait GetF64Vec {
+    fn get_f64_vec(&self) -> Result<Vec<f64>>;
+}
+
+impl GetF64 for Value {
+    fn get_f64(&self) -> Result<f64> {
+        self.as_f64().ok_or(ParseError::InvalidValue)
+    }
+}
+
+impl GetF64Vec for Value {
+    fn get_f64_vec(&self) -> Result<Vec<f64>> {
+        self.as_sequence()
+            .ok_or(ParseError::InvalidValue)?
+            .iter()
+            .map(|f| f.get_f64())
+            .collect::<Result<Vec<f64>>>()
+    }
+}
+
+fn construct_camera(description: &Mapping) -> Result<Camera> {
+    let width = description
+        .get("width")
+        .ok_or(ParseError::InvalidCameraParams(
+            "Missing camera width".to_string(),
+        ))?
+        .as_u64()
+        .ok_or(ParseError::InvalidCameraParams(
+            "Camera width must be an integer".to_string(),
+        ))? as usize;
+
+    let height = description
+        .get("height")
+        .ok_or(ParseError::InvalidCameraParams(
+            "Missing camera height".to_string(),
+        ))?
+        .as_u64()
+        .ok_or(ParseError::InvalidCameraParams(
+            "Camera height must be an integer".to_string(),
+        ))? as usize;
+
+    let field_of_view = description
+        .get("field-of-view")
+        .ok_or(ParseError::InvalidCameraParams(
+            "Missing field-of-view".to_string(),
+        ))?
+        .get_f64()?;
+
+    let from_values = description
+        .get("from")
+        .ok_or(ParseError::InvalidCameraParams(
+            "Missing 'from'".to_string(),
+        ))?
+        .get_f64_vec()?;
+
+    let to_values = description
+        .get("to")
+        .ok_or(ParseError::InvalidCameraParams("Missing 'to'".to_string()))?
+        .get_f64_vec()?;
+
+    let up_values = description
+        .get("up")
+        .ok_or(ParseError::InvalidCameraParams("Missing 'up'".to_string()))?
+        .get_f64_vec()?;
+
+    let from = Tuple::point(from_values[0], from_values[1], from_values[2]);
+    let to = Tuple::point(to_values[0], to_values[1], to_values[2]);
+    let up = Tuple::vector(up_values[0], up_values[1], up_values[2]);
+
+    let transformation = compute_view_transformation(from, to, up)?;
+    let camera = Camera::new(width, height, field_of_view).with_transformation(transformation);
+
+    Ok(camera)
+}
+
+fn construct_light(description: &Mapping) -> Result<Light> {
+    let position_values = description
+        .get("at")
+        .ok_or(ParseError::InvalidLightParams("Missing 'at'".to_string()))?
+        .get_f64_vec()?;
+
+    let intensity_values = description
+        .get("intensity")
+        .ok_or(ParseError::InvalidLightParams(
+            "Missing 'intensity'".to_string(),
+        ))?
+        .get_f64_vec()?;
+
+    let position = Tuple::point(position_values[0], position_values[1], position_values[2]);
+    let intensity = Color::new(
+        intensity_values[0],
+        intensity_values[1],
+        intensity_values[2],
+    );
+
+    Ok(Light::new(position, intensity))
+}
+
+fn parse_material(description: &Value) -> Material {
+    let color_values = description
+        .get("color")
+        .and_then(|x| x.get_f64_vec().ok())
+        .unwrap_or_else(|| vec![1., 1., 1.]);
+
+    let color = Color::new(color_values[0], color_values[1], color_values[2]);
+
+    let ambient = get_or_default_f64!(description, "ambient", 0.1);
+    let diffuse = get_or_default_f64!(description, "diffuse", 0.9);
+    let specular = get_or_default_f64!(description, "specular", 0.9);
+    let shininess = get_or_default_f64!(description, "shininess", 200.);
+    let reflectivity = get_or_default_f64!(description, "reflectivity", 0.);
+    let transparency = get_or_default_f64!(description, "transparency", 0.);
+    let refractive_index = get_or_default_f64!(description, "refractive-index", 1.);
+    let casts_shadow = description
+        .get("casts-shadow")
+        .and_then(|x| x.as_bool())
+        .unwrap_or(true);
+
+    Material {
+        ambient,
+        diffuse,
+        color,
+        specular,
+        shininess,
+        reflectivity,
+        transparency,
+        refractive_index,
+        casts_shadow,
+        ..Default::default()
+    }
+}
+
+pub fn construct_object(object_type: &str, description: &Mapping) -> Result<Box<dyn Shape>> {
+    let material = if let Some(material_description) = description.get("material") {
+        parse_material(material_description)
+    } else {
+        Material::default()
+    };
+
+    let mut transformation = Matrix::identity();
+
+    if let Some(transformations) = description.get("transform").and_then(|x| x.as_sequence()) {
+        for value in transformations {
+            let key = value
+                .get(0)
+                .ok_or(ParseError::InvalidValue)?
+                .as_str()
+                .ok_or(ParseError::InvalidValue)?;
+
+            let value = value.get(1).ok_or(ParseError::InvalidValue)?;
+
+            if value.is_sequence() {
+                let value = value.get_f64_vec()?;
+
+                match key {
+                    "scale" => {
+                        transformation = transformation.scale(value[0], value[1], value[2]);
+                    }
+                    "translate" => {
+                        transformation = transformation.translate(value[0], value[1], value[2]);
+                    }
+                    &_ => unimplemented!(),
+                }
+            } else {
+                let radians = value.get_f64()?;
+
+                match key {
+                    "rotate-x" => {
+                        transformation = transformation.rotate_x(radians);
+                    }
+                    "rotate-y" => {
+                        transformation = transformation.rotate_y(radians);
+                    }
+                    "rotate-z" => {
+                        transformation = transformation.rotate_z(radians);
+                    }
+                    _ => unimplemented!(),
+                }
+            }
+        }
+    }
+
+    dbg!(&transformation);
+
+    let object: Box<dyn Shape> = match object_type {
+        "sphere" => Box::new(sphere::Sphere::new(transformation, material)),
+        "plane" => Box::new(plane::Plane::new(transformation, material)),
+        "cube" => Box::new(cube::Cube::new(transformation, material)),
+        _ => unimplemented!(),
+    };
+
+    Ok(object)
+}
+
+pub fn parse_scene(input: &str) -> Result<Scene> {
+    let sequence = serde_yaml::from_str::<Sequence>(input)?;
+
+    let mut camera: Option<Camera> = None;
+    let mut lights = vec![];
+    let mut objects: Vec<Box<dyn Shape>> = vec![];
+
+    for item in sequence {
+        if let Some(mapping) = item.as_mapping() {
+            if let Some(Value::String(item_type)) = mapping.get("add") {
+                match item_type.as_str() {
+                    "camera" => {
+                        camera = Some(construct_camera(mapping)?);
+                    }
+                    "light" => {
+                        let light = construct_light(mapping)?;
+                        lights.push(light);
+                    }
+                    "sphere" | "plane" | "cube" => {
+                        let object = construct_object(item_type.as_str(), mapping)?;
+                        objects.push(object);
+                    }
+                    _ => return Err(ParseError::UnknownItem(item_type.to_owned())),
+                }
+            }
+        } else {
+            return Err(ParseError::InvalidYamlObject(format!(
+                "Expected mapping, found {item:#?}"
+            )));
+        }
+    }
+
+    let camera = camera.expect("The scene must define a camera!");
+
+    let scene = Scene {
+        camera,
+        lights,
+        objects,
+    };
+    Ok(scene)
+}
--- rt_run/src/main.rs
+++ rt_run/src/main.rs
@@ -1,153 +1,33 @@
-use std::{f64::consts::PI, fs::File, io::Write};
-
-use ray_tracer::{
-    camera,
-    color::Color,
-    light::PointLight,
-    material::Material,
-    pattern::*,
-    shape::{plane::Plane, sphere::Sphere},
-    transformation,
-    world::World,
-    Matrix, Tuple,
+use std::{
+    env,
+    fs::File,
+    io::{Read, Write},
 };
 
+use parse_scene::parse_scene;
+use ray_tracer::world::World;
+
 fn main() {
-    let solid_pattern1 = Box::new(solid::SolidPattern::new(Color::new(0.8, 0.8, 1.0)));
-    let solid_pattern2 = Box::new(solid::SolidPattern::new(Color::from_rgb(32, 178, 170)));
+    let mut args = env::args();
 
-    let blended_pattern1 =
-        blended::BlendedPattern::new(solid_pattern1, solid_pattern2, Matrix::identity());
+    let yaml_filename = args
+        .nth(1)
+        .expect("Must provide a path to a YAML file with a scene description.");
+    let mut file = File::open(yaml_filename).expect("The file should exist and be openable");
+    let mut data = String::new();
+    file.read_to_string(&mut data)
+        .expect("The file should be readable to a string");
 
-    let solid_pattern1 = Box::new(solid::SolidPattern::new(Color::from_rgb(100, 40, 77)));
-    let solid_pattern2 = Box::new(solid::SolidPattern::new(Color::from_rgb(7, 200, 90)));
+    let scene = parse_scene(data.as_str()).expect("Should be able to parse scene YAML");
 
-    let blended_pattern2 =
-        blended::BlendedPattern::new(solid_pattern1, solid_pattern2, Matrix::identity());
+    let world = World::new(scene.objects, scene.lights.get(0).cloned());
 
-    let floor_pattern1 = checker::CheckerPattern::new(
-        Box::new(blended_pattern1),
-        Box::new(blended_pattern2),
-        Matrix::identity(),
-    );
-
-    let solid_pattern1 = solid::SolidPattern::new(Color::from_rgb(100, 200, 50));
-    let solid_pattern2 = solid::SolidPattern::new(Color::from_rgb(13, 5, 70));
-
-    let floor_pattern2 = checker::CheckerPattern::new(
-        Box::new(solid_pattern1),
-        Box::new(solid_pattern2),
-        Matrix::identity(),
-    );
-
-    let floor_pattern = checker::CheckerPattern {
-        a: Box::new(floor_pattern1),
-        b: Box::new(floor_pattern2),
-        ..Default::default()
-    };
-    let floor_material = Material {
-        pattern: Some(Box::new(floor_pattern)),
-        reflectivity: 0.6,
-        ..Default::default()
-    };
-    let floor = Plane::new()
-        .with_material(floor_material)
-        .with_transformation(Matrix::scaling(3., 3., 3.));
-
-    let wall_transformation = Matrix::identity().rotate_x(PI / 2.).translate(0., 0., 3.);
-    let wall_material = Material {
-        reflectivity: 1.,
-        ..Default::default()
-    };
-    let wall = Plane::new()
-        .with_transformation(wall_transformation)
-        .with_material(wall_material);
-
-    let middle_sphere_transformation = Matrix::translation(-0.5, 1., 0.5);
-    // let middle_sphere_pattern = Box::new(SolidPattern::new(Color::from_rgb(75, 0, 130)));
-
-    let middle_sphere_material = Material {
-        color: Color::from_rgb(75, 0, 130),
-        transparency: 0.9,
-        reflectivity: 1.,
-        diffuse: 0.1,
-        specular: 1.,
-        shininess: 300.,
-        ambient: 0.1,
-        pattern: None,
-        casts_shadow: false,
-        ..Default::default()
-    };
-    let middle_sphere = Sphere::default()
-        .with_material(middle_sphere_material)
-        .with_transformation(middle_sphere_transformation);
-
-    let right_sphere_transformation = Matrix::identity()
-        .scale(0.5, 0.5, 0.5)
-        .translate(1.5, 0.5, -0.5);
-    let right_sphere_material = Material {
-        color: Color::new(0.5, 1., 0.1),
-        diffuse: 0.7,
-        specular: 0.3,
-        reflectivity: 0.5,
-        pattern: Some(Box::new(gradient::GradientPattern {
-            a: Color::from_rgb(218, 112, 214),
-            b: Color::from_rgb(75, 0, 130),
-            transformation: Matrix::scaling(0.5, 0.5, 0.5),
-        })),
-        ..Default::default()
-    };
-    let right_sphere = Sphere::default()
-        .with_transformation(right_sphere_transformation)
-        .with_material(right_sphere_material);
-
-    let left_sphere_subpattern = radial_gradient::RadialGradientPattern::new(
-        Color::new(0.1, 0., 0.9),
-        Color::new(1., 0., 1.),
-        Matrix::identity(),
-    );
-
-    let left_sphere_pattern = perturbed::PerturbedPattern::new(
-        Box::new(left_sphere_subpattern),
-        Matrix::scaling(0.2, 0.2, 0.2),
-    );
-
-    let left_sphere_transformation = Matrix::scaling(0.33, 0.33, 0.33).translate(-1.5, 0.33, -0.75);
-    let left_sphere_material = Material {
-        color: Color::new(1., 0.8, 0.1),
-        diffuse: 0.7,
-        specular: 0.3,
-        pattern: Some(Box::new(left_sphere_pattern)),
-        ..Default::default()
-    };
-    let left_sphere = Sphere::default()
-        .with_transformation(left_sphere_transformation)
-        .with_material(left_sphere_material);
-
-    let world = World {
-        light: Some(PointLight::new(
-            Tuple::point(-10., 10., -10.),
-            Color::white(),
-        )),
-        objects: vec![
-            Box::new(floor),
-            Box::new(wall),
-            Box::new(left_sphere),
-            Box::new(middle_sphere),
-            Box::new(right_sphere),
-        ],
-    };
-
-    let camera_transformation = transformation::compute_view_transformation(
-        Tuple::point(0., 5., -7.),
-        Tuple::point(0., 2., 0.),
-        Tuple::vector(0., 1., 0.),
-    )
-    .expect("Should compute camera transformation");
-    let camera = camera::Camera::new(1000, 500, PI / 3.).with_transformation(camera_transformation);
-    let canvas = camera.render(&world).expect("Should render the image");
+    let canvas = scene
+        .camera
+        .render(&world)
+        .expect("Should be able to render the scene");
 
     let ppm = canvas.to_ppm();
-    let mut buffer = File::create("world.ppm").unwrap();
+    let mut buffer = File::create("scene.ppm").unwrap();
     buffer.write_all(ppm.as_bytes()).unwrap();
 }

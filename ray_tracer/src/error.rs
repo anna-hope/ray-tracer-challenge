114aebcd460439d237b6863694a8c4a747cc2a45
Anna Melnikov <anna.me@hey.com>
2023-04-18 17:02:09 -0500

    refactor: remove unused error type
    Remove NonVectorTuple error, since we now have a distinct Vector type.

--- ray_tracer/src/error.rs
+++ ray_tracer/src/error.rs
@@ -2,9 +2,6 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum RayTracerError {
-    #[error("Attempt to use a non-vector tuple in a vector-only context")]
-    NonVectorTuple,
-
     #[error("Attempted to calculate an inverse of a non-invertible matrix")]
     NonInvertibleMatrix,
 }
